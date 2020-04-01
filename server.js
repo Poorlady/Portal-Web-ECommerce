916157e040f6609d6a05fc8ab7dc0a3f5112918b
Poorlady <saniekasmara@gmail.com>
2020-04-01 16:27:24 +0700

    make it better

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ server.use(fileUpload());
 server.use(apiRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  server.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  server.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
