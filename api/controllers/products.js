e067c4be6f44bd4e8c963c1d68949f909d80c0ba
Poorlady <saniekasmara@gmail.com>
2020-06-01 21:29:37 +0700

    end

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -27,14 +27,13 @@ const handleFile = (file, imgIndex, storeId, name, formerImg) => {
     file.name.replace(/\s/g, `${storeId + imgIndex + name}`);
 
   fileContainer.mv(`${mainpath}/client/public/uploads/products/${imgName}`);
-
-  formerImg !== "null" && formerImg !== "undefined"
-    ? removeFile(formerImg)
-    : null;
+  console.log(formerImg);
+  formerImg !== "null" ? removeFile(formerImg) : null;
   return imgName;
 };
 
 const setData = (req) => {
+  console.log(req.body);
   const {
     name,
     desc,
@@ -124,7 +123,7 @@ const setData = (req) => {
 exports.postProduct = (req, res) => {
   const dataSet = setData(req);
   const product = new Product(dataSet);
-
+  console.log(dataSet);
   product
     .save()
     .then((result) => {
@@ -180,7 +179,7 @@ exports.getProductsByStoreId = (req, res) => {
 
 exports.editProduct = (req, res) => {
   const dataSet = setData(req);
-
+  console.log(dataSet);
   Product.findOneAndUpdate(
     { _id: req.params.id },
     {
